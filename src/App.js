3506cbef2b7eaa0ac6f1eae3a16102826157ed6b
alexarg87923 <alexarguelles87923@gmail.com>
2023-02-26 19:22:14 -0500

    STARTING WEBSITE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+const headerStyle = { 
+  marginTop: '71px',
+  paddingLeft: '71px',
+  color:"red"
+};
+const margin = { 
+  marginTop: '71px'
+};
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn test
-        </a>
-      </header>
+
+    <div style={margin} className="App">
+
+      <a href="/about">
+        TEST
+      </a>
+
+      <a href="/events" style={headerStyle}>
+        TEST
+      </a>
+
+      <a href="https://sharknet.mdc.edu/organization/witmdc" style={headerStyle}>
+        TEST
+      </a>
+
+
     </div>
+    
   );
 }
+
+
 
 export default App;
